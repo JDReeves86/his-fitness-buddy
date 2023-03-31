387a30b0f9de47e232ad01dc03605bba8103c77f
JDReeves86 <Reeves.Jacobd@gmail.com>
2023-03-31 14:07:12 -0700

    converting search bar to buttons

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -11,37 +11,101 @@ interface Props {
 const baseAPIURL: string = "https://wger.de/api/v2/exercise?language=2&";
 
 function Searchbar({ action }: Props) {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleChange = (event: any) => {
-    const {
-      target: { value },
-    } = event;
-    console.log(value);
-    setSearchTerm(value);
-  };
-
-  const sendRequest = (data: string) => {
+  const sendRequest = (event: any) => {
+    const data = event.target.value;
     console.log(`searching for ${data}`);
-    findMuscle(data)
+    findMuscle(data);
   };
   return (
-    <>
-      <input
-        className={styles.searchInput}
-        type="text"
-        placeholder="Enter a search term"
-        onChange={handleChange}
-      ></input>
+    <div className={styles.btnList}>
       <button
         className={styles.searchBtn}
-        onClick={() => {
-          sendRequest(searchTerm);
-        }}
+        value="Pectoralis major"
+        onClick={sendRequest}
       >
-        Search
+        Chest
       </button>
-    </>
+      <button
+        className={styles.searchBtn}
+        value="Latissimus dorsi"
+        onClick={sendRequest}
+      >
+        Back
+      </button>
+      <button
+        className={styles.searchBtn}
+        value="Anterior deltoid"
+        onClick={sendRequest}
+      >
+        Shoulders
+      </button>
+      <button
+        className={styles.searchBtn}
+        value="Biceps brachii"
+        onClick={sendRequest}
+      >
+        Biceps
+      </button>
+      <button
+        className={styles.searchBtn}
+        value="Triceps brachii"
+        onClick={sendRequest}
+      >
+        Triceps
+      </button>
+      <button
+        className={styles.searchBtn}
+        value="Quadriceps femoris"
+        onClick={sendRequest}
+      >
+        Quadriceps
+      </button>
+      <button
+        className={styles.searchBtn}
+        value="Biceps femoris"
+        onClick={sendRequest}
+      >
+        Hamstrings
+      </button>
+      <button
+        className={styles.searchBtn}
+        value="Gluteus maximus"
+        onClick={sendRequest}
+      >
+        Glutes
+      </button>
+      <button
+        className={styles.searchBtn}
+        value="Rectus abdominis"
+        onClick={sendRequest}
+      >
+        Abs
+      </button>
+      <button
+        className={styles.searchBtn}
+        value={[
+          "Gastrocnemius",
+          "Gluteus maximus",
+          "Quadriceps femoris",
+          "Soleus",
+        ]}
+        onClick={sendRequest}
+      >
+        Lower Body
+      </button>
+      <button
+        className={styles.searchBtn}
+        value={[
+          "Gastrocnemius",
+          "Gluteus maximus",
+          "Quadriceps femoris",
+          "Soleus",
+        ]}
+        onClick={sendRequest}
+      >
+        Upper Body
+      </button>
+    </div>
   );
 }
 
